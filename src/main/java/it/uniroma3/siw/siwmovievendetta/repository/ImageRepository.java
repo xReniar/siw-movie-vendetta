122c672cfa54c69b747b4f27d307c7a1c876cb07
xReniar <ilaorainer@gmail.com>
2023-05-09 16:50:47 +0200

    created Image repository

---
+++ src/main/java/it/uniroma3/siw/siwmovievendetta/repository/ImageRepository.java
@@ -0,0 +1,7 @@
+package it.uniroma3.siw.siwmovievendetta.repository;
+
+import it.uniroma3.siw.siwmovievendetta.model.Image;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ImageRepository extends CrudRepository<Image,Long> {
+}

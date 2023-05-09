8300e1d217beec12991c1a2da58cda2c083b19ae
xReniar <ilaorainer@gmail.com>
2023-05-09 16:50:24 +0200

    .

---
+++ src/main/java/it/uniroma3/siw/siwmovievendetta/SiwMovieVendettaApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siwmovievendetta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwMovieVendettaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwMovieVendettaApplication.class, args);
+	}
+
+}

264757c895ff361cdb979e65c844f49cd30f3474
xReniar <ilaorainer@gmail.com>
2023-05-10 16:55:46 +0200

    modified Movie class

--- src/main/java/it/uniroma3/siw/siwmovievendetta/model/Movie.java
+++ src/main/java/it/uniroma3/siw/siwmovievendetta/model/Movie.java
@@ -1,7 +1,11 @@
 package it.uniroma3.siw.siwmovievendetta.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -10,10 +14,15 @@ public class Movie {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank
+    private String title;
+
+    @Min(1900)
+    @Max(2023)
     private Integer year;
 
-    @OneToMany
-    List<Image> images;
+    @ManyToMany()
+    List<Picture> pictures;
 
     public Long getId() {
         return id;
@@ -21,6 +30,14 @@ public class Movie {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public Integer getYear() {
@@ -31,11 +48,11 @@ public class Movie {
         this.year = year;
     }
 
-    public List<Image> getImages() {
-        return images;
+    public List<Picture> getImages() {
+        return pictures;
     }
 
-    public void setImages(List<Image> images) {
-        this.images = images;
+    public void setImages(List<Picture> pictures) {
+        this.pictures = pictures;
     }
 }

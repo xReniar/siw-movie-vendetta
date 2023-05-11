37d939525d2e7d640e5cb515d7a61c7f3df1625b
xReniar <ilaorainer@gmail.com>
2023-05-11 10:45:32 +0200

    created artist class

---
+++ src/main/java/it/uniroma3/siw/siwmovievendetta/model/Artist.java
@@ -0,0 +1,96 @@
+package it.uniroma3.siw.siwmovievendetta.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class Artist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String surname;
+
+    @NotBlank
+    private LocalDate birthDate;
+    private LocalDate deathDate;
+
+    @OneToOne
+    private Picture picture;
+
+    @OneToMany(mappedBy = "director")
+    private List<Movie> directedMovies;
+
+    @ManyToMany
+    private List<Movie> actedMovies;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public LocalDate getDeathDate() {
+        return deathDate;
+    }
+
+    public void setDeathDate(LocalDate deathDate) {
+        this.deathDate = deathDate;
+    }
+
+    public Picture getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Picture picture) {
+        this.picture = picture;
+    }
+
+    public List<Movie> getDirectedMovies() {
+        return directedMovies;
+    }
+
+    public void setDirectedMovies(List<Movie> directedMovies) {
+        this.directedMovies = directedMovies;
+    }
+
+    public List<Movie> getActedMovies() {
+        return actedMovies;
+    }
+
+    public void setActedMovies(List<Movie> actedMovies) {
+        this.actedMovies = actedMovies;
+    }
+}

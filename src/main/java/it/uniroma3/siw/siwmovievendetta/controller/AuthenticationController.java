55e4b82b56f2183da61bdff46f16f0b3186939c9
xReniar <ilaorainer@gmail.com>
2023-05-16 11:38:06 +0200

    added mapping requests

--- src/main/java/it/uniroma3/siw/siwmovievendetta/controller/AuthenticationController.java
+++ src/main/java/it/uniroma3/siw/siwmovievendetta/controller/AuthenticationController.java
@@ -1,8 +1,10 @@
 package it.uniroma3.siw.siwmovievendetta.controller;
 
+import it.uniroma3.siw.siwmovievendetta.controller.validator.UserValidator;
 import it.uniroma3.siw.siwmovievendetta.model.Credentials;
 import it.uniroma3.siw.siwmovievendetta.model.User;
 import it.uniroma3.siw.siwmovievendetta.service.CredentialsService;
+import it.uniroma3.siw.siwmovievendetta.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -20,6 +22,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class AuthenticationController {
     @Autowired
     private CredentialsService credentialsService;
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/index")
     public String index(){
@@ -62,9 +67,10 @@ public class AuthenticationController {
                                @ModelAttribute("credentials") Credentials credentials,
                                BindingResult credentialsBindingResult,
                                Model model) {
-
+        //this.userValidator.validate(user,userBindingResult);
         // se user e credential hanno entrambi contenuti validi, memorizza User e the Credentials nel DB
         if(!userBindingResult.hasErrors() && ! credentialsBindingResult.hasErrors()) {
+            userService.saveUser(user);
             credentials.setUser(user);
             credentialsService.saveCredentials(credentials);
             model.addAttribute("user", user);
@@ -72,6 +78,4 @@ public class AuthenticationController {
         }
         return "formRegister.html";
     }
-
-
 }

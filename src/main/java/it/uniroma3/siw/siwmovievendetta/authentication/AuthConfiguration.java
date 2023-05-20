d82e25a24d4db91bba8d5af3e5c486321fb8c171
xReniar <ilaorainer@gmail.com>
2023-05-20 20:43:30 +0200

    added mapping requests for all user

--- src/main/java/it/uniroma3/siw/siwmovievendetta/authentication/AuthConfiguration.java
+++ src/main/java/it/uniroma3/siw/siwmovievendetta/authentication/AuthConfiguration.java
@@ -36,7 +36,10 @@ public class AuthConfiguration {
                 .authorizeHttpRequests((requests) -> {
                             try {
                                 requests
-                                        .requestMatchers(HttpMethod.GET,"/index","/", "/login","/register","/movies","/artists","/css/**", "/images/**").permitAll()
+                                        .requestMatchers(HttpMethod.GET,"/index","/", "/login","/register",
+                                                                                    "/movies","/movie/{movieId}",
+                                                                                    "/artists","/artist/{artistId}",
+                                                                                    "/css/**", "/images/**").permitAll()
                                         .requestMatchers(HttpMethod.POST, "/login", "/register").permitAll()
                                         // solo gli utenti autenticati con ruolo ADMIN possono accedere a risorse con path /admin/**
                                         .requestMatchers(HttpMethod.GET,"/admin/**").hasAnyAuthority(ADMIN_ROLE)

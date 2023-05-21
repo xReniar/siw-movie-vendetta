8b4fcbeb1f6a58f79cc601d973fc18e47063cdd1
xReniar <ilaorainer@gmail.com>
2023-05-22 00:14:35 +0200

    added find by year but should be title instead

--- src/main/java/it/uniroma3/siw/siwmovievendetta/repository/MovieRepository.java
+++ src/main/java/it/uniroma3/siw/siwmovievendetta/repository/MovieRepository.java
@@ -1,8 +1,13 @@
 package it.uniroma3.siw.siwmovievendetta.repository;
 
 import it.uniroma3.siw.siwmovievendetta.model.Movie;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface MovieRepository extends CrudRepository<Movie,Long> {
     public boolean existsByTitleAndYear(String title,Integer year);
+
+    public List<Movie> findByYear(Integer year);
 }

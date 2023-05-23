be953abb56a50b3aaad2e82f284191facbb50d05
xReniar <ilaorainer@gmail.com>
2023-05-23 12:21:49 +0200

    added find by title function

--- src/main/java/it/uniroma3/siw/siwmovievendetta/repository/MovieRepository.java
+++ src/main/java/it/uniroma3/siw/siwmovievendetta/repository/MovieRepository.java
@@ -10,4 +10,6 @@ public interface MovieRepository extends CrudRepository<Movie,Long> {
     public boolean existsByTitleAndYear(String title,Integer year);
 
     public List<Movie> findByYear(Integer year);
+
+    public List<Movie> findByTitle(String title);
 }

56f0952286a787ebb8b08260c9f5802a98416f1b
xReniar <ilaorainer@gmail.com>
2023-05-23 12:19:28 +0200

    added function for retrieving info for movies

--- src/main/java/it/uniroma3/siw/siwmovievendetta/repository/ArtistRepository.java
+++ src/main/java/it/uniroma3/siw/siwmovievendetta/repository/ArtistRepository.java
@@ -1,10 +1,16 @@
 package it.uniroma3.siw.siwmovievendetta.repository;
 
 import it.uniroma3.siw.siwmovievendetta.model.Artist;
+import it.uniroma3.siw.siwmovievendetta.model.Movie;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
-import java.time.LocalDate;
 
 public interface ArtistRepository extends CrudRepository<Artist,Long> {
     public boolean existsByNameAndSurname(String name, String surname);
+
+    public List<Artist> getByDirectedMoviesNotContaining(Movie movie);
+    public List<Artist> getByActedMoviesNotContaining(Movie movie);
 }

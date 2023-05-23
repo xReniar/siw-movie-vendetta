497923cef74fc9e4894a5b916fdbc8fe31d5341c
xReniar <ilaorainer@gmail.com>
2023-05-23 18:27:06 +0200

    added back to movie in form update

--- src/main/java/it/uniroma3/siw/siwmovievendetta/controller/MovieController.java
+++ src/main/java/it/uniroma3/siw/siwmovievendetta/controller/MovieController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Set;
 
 @Controller
@@ -85,9 +84,12 @@ public class MovieController {
         model.addAttribute("movie",movie);
 
         Set<Artist> cast = movie.getActors();
-        cast.add(movie.getDirector());
+        if(movie.getDirector() != null){
+            cast.add(movie.getDirector());
+        }
 
         model.addAttribute("cast", cast);
+        model.addAttribute("director", movie.getDirector());
 
         return "movie.html";
     }

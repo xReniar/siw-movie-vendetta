a24c27e4c4c34fd4d613cdf868963142874f7e38
xReniar <ilaorainer@gmail.com>
2023-05-26 21:29:27 +0200

    removed base 64 image

--- src/main/java/it/uniroma3/siw/siwmovievendetta/model/Image.java
+++ src/main/java/it/uniroma3/siw/siwmovievendetta/model/Image.java
@@ -2,28 +2,20 @@ package it.uniroma3.siw.siwmovievendetta.model;
 
 import jakarta.persistence.*;
 
-import java.util.Base64;
-
 @Entity
 public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    /*
-    @Lob
     private byte[] bytes;
-     */
-
-    @Lob
-    private String base64Image;
 
     public Image(){
 
     }
 
     public Image(byte[] bytes){
-        this.setBase64Image(Base64.getEncoder().encodeToString(bytes));
+        this.bytes = bytes;
     }
 
     public Long getId() {
@@ -34,21 +26,11 @@ public class Image {
         this.id = id;
     }
 
-    /*
     public byte[] getBytes() {
         return bytes;
     }
 
     public void setBytes(byte[] bytes) {
         this.bytes = bytes;
-    }
-     */
-
-    public String getBase64Image() {
-        return base64Image;
-    }
-
-    public void setBase64Image(String base64Image) {
-        this.base64Image = base64Image;
     }
 }

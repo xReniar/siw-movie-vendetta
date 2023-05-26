0710b7fd94d61d5ddb19d63f5ce59ee11b360b03
xReniar <ilaorainer@gmail.com>
2023-05-26 21:28:19 +0200

    .

--- src/main/java/it/uniroma3/siw/siwmovievendetta/repository/CredentialsRepository.java
+++ src/main/java/it/uniroma3/siw/siwmovievendetta/repository/CredentialsRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface CredentialsRepository extends CrudRepository<Credentials,Long> {
     public Optional<Credentials> findByUsername(String username);
+
+    public boolean existsByUsername(String username);
 }

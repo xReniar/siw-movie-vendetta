4e5c1a7b826f05c491f4589310e1c6ce5a381704
xReniar <ilaorainer@gmail.com>
2023-05-26 21:28:02 +0200

    added userValidator and credentialsValidator

--- src/main/java/it/uniroma3/siw/siwmovievendetta/controller/AuthenticationController.java
+++ src/main/java/it/uniroma3/siw/siwmovievendetta/controller/AuthenticationController.java
@@ -1,5 +1,6 @@
 package it.uniroma3.siw.siwmovievendetta.controller;
 
+import it.uniroma3.siw.siwmovievendetta.controller.validator.CredentialsValidator;
 import it.uniroma3.siw.siwmovievendetta.controller.validator.UserValidator;
 import it.uniroma3.siw.siwmovievendetta.model.Credentials;
 import it.uniroma3.siw.siwmovievendetta.model.User;
@@ -22,6 +23,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class AuthenticationController {
     @Autowired
     private CredentialsService credentialsService;
+
+    @Autowired
+    private UserValidator userValidator;
+
+    @Autowired
+    private CredentialsValidator credentialsValidator;
 
     @Autowired
     private UserService userService;
@@ -67,9 +74,10 @@ public class AuthenticationController {
                                @ModelAttribute("credentials") Credentials credentials,
                                BindingResult credentialsBindingResult,
                                Model model) {
-        //this.userValidator.validate(user,userBindingResult);
+        this.userValidator.validate(user,userBindingResult);
+        this.credentialsValidator.validate(credentials, credentialsBindingResult);
         // se user e credential hanno entrambi contenuti validi, memorizza User e the Credentials nel DB
-        if(!userBindingResult.hasErrors() && ! credentialsBindingResult.hasErrors()) {
+        if(!userBindingResult.hasErrors() && !credentialsBindingResult.hasErrors()) {
             userService.saveUser(user);
             credentials.setUser(user);
             credentialsService.saveCredentials(credentials);
